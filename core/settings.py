cb2c8f6d377a9e4b65b6bca468229e2cec0071e3
mahdighadiriii <mahdighadiriafzal@gmail.com>
2023-10-17 21:41:09 +0330

    Update

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lsk++uc15a_2mh38alqhauv#@1pz%%1jj!gb&0ouob1$p#jq4g'
+SECRET_KEY = (
+    "django-insecure-lsk++uc15a_2mh38alqhauv#@1pz%%1jj!gb&0ouob1$p#jq4g"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- todo/models.py
+++ todo/models.py
@@ -10,7 +10,7 @@ class Task(models.Model):
     )
     title = models.CharField(max_length=200)
     complete = models.BooleanField(default=False)
-    created_date =  models.DateTimeField(auto_now_add=True)
+    created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
